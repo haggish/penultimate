107ffebf3d9118aeb1b6d96182bef017013b3935
Mika Salminen <misalminen@team.mobile.de>
2015-08-11 10:00:10 +0200

    Unit tests for people

--- src/test/java/org/katastrofi/penultimate/WorldTest.java
+++ src/test/java/org/katastrofi/penultimate/WorldTest.java
@@ -1,23 +1,53 @@
 package org.katastrofi.penultimate;
 
+import static java.util.Optional.empty;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.katastrofi.penultimate.Location.coord;
+
+import java.util.Optional;
+
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class WorldTest {
 
+    private World instance;
+
+    @Before
+    public void init() {
+        this.instance = TestData.oneFloor();
+    }
+
     @Test
     public void thingsAtAddedThingsLocationReturnAddedThings() {
+        Stone stone = new Stone(instance);
 
+        assertThat(instance.thingsAt(coord(1, 1)), contains(stone));
     }
 
     @Test
     public void
     thingByNameAndPlaceIsTheThingInGivenPlaceByGivenName() {
+        Stone stone = new Stone(instance);
 
+        assertThat(instance.thingByNameAndPlace(stone.genericName(), coord(1, 1)),
+                is(Optional.of(stone)));
     }
 
     @Test
     public void thingByNameAndPlaceIsEmptyIfNoSuchThingInGivenPlace() {
+        assertThat(instance.thingByNameAndPlace("Stone", coord(1, 1)),
+                is(empty()));
+    }
 
+    @Test
+    public void randomLocationOfGivenTerrainTypeIsImplementedByGivenTerrain() {
+        Location location = instance.randomLocationOf(Floor.class); // "random" selection of one fragment
+        assertThat(instance.terrain().fragmentAt(location), is(instanceOf(Floor.class)));
     }
 
 }
